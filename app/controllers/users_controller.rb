ab3085c019d9ea7785e28973f01c43bd9edb83c2
CodeIgor31 <yagmurov.igor@mail.ru>
2022-12-18 17:46:02 +0300

    Added mails but only by letter_opener and some fixes with dates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UserMailer.with(user: @user).welcome_email.deliver_now
       session[:user_id] = @user.id
       flash[:success] = "Добро пожаловать, #{@user.first_name}!"
       redirect_to home_path
