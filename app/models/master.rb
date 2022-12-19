c770491d18096b70e4a1efe87a6d3dbae6292913
CodeIgor31 <yagmurov.igor@mail.ru>
2022-12-19 15:16:00 +0300

    More front

--- app/models/master.rb
+++ app/models/master.rb
@@ -1,3 +1,8 @@
 class Master < ApplicationRecord
     has_many :service_records
+    has_one_attached :face
+
+    def mas_thumb(pic)
+        pic.variant(resize_to_limit: [300, 300]).processed
+    end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -3,7 +3,7 @@ class Work < ApplicationRecord
     belongs_to :user
     enum male: [:women, :man]
     validates :age, presence: true
-    validates_numericality_of :age, only_integer: true, greater_than_or_equal_to: 18
+    validates_numericality_of :age, only_integer: true, greater_than_or_equal_to: 18, less_than_or_equal_to: 70
     validates :male, presence: true
     validates :task_1, :task_2, :task_3, presence: true, length: {minimum: 10}
 end
